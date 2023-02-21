7aaec7bb79e1380bf6fb5f8da3f616778e2a32ce
yoga980393 <wggisdde@gmail.com>
2023-02-21 11:53:37 +0800

    變很短

--- OnClass_20230221_01/Program.cs
+++ OnClass_20230221_01/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,24 +13,8 @@ namespace OnClass_20230221_01
         {
             Console.WriteLine("輸入帶,的字串");
             var list = Console.ReadLine().Split(',').ToList();
-            var count = list.Count;
-            var list2 = new List<string>();
-
-            for(int i = 0; i < count; i++)
-            {
-                list2.Add(list.Skip(count - i - 1).Take(1).First());
-            }
-            
-            int n = 0;
-            foreach(var item in list2)
-            {
-                Console.Write(item);
-                n++;
-                if(n < count)
-                {
-                    Console.Write(", ");
-                }
-            }
+            var list2 = list.OrderByDescending(x => list.IndexOf(x));
+            Console.WriteLine(string.Join(",", list2));
         }
     }
 }
