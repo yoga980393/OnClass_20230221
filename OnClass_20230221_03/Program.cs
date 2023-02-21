47056963b37b3e44a4c08a96f8baa0400c5a951f
yoga980393 <wggisdde@gmail.com>
2023-02-21 15:04:17 +0800

    第三題不會

---
+++ OnClass_20230221_03/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnClass_20230221_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("輸入一個數字：");
+            int r = int.Parse(Console.ReadLine());
+            var list = new List<int>();
+            for(int i = 0; i < r; i++)
+            {
+                list.Add(r - i);
+            }
+
+            foreach(var item in list)
+            {
+                for(int i = 0; i < list.IndexOf(item) + 1; i++)
+                {
+                    Console.Write(item);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
