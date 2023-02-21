bf54929f0c0af491a364360238bbe02b3d088fca
yoga980393 <wggisdde@gmail.com>
2023-02-21 13:17:46 +0800

    比較潮

--- OnClass_20230221_02/Program.cs
+++ OnClass_20230221_02/Program.cs
@@ -13,7 +13,7 @@ namespace OnClass_20230221_02
             Console.WriteLine("輸入帶,的字串");
             var list = Console.ReadLine().Split(',');
             var list2 = list.Where(x => int.Parse(x) % 2 == 0).OrderBy(x => x);
-            var list3 = list.Where(x => int.Parse(x) % 2 == 1).OrderBy(x => x);
+            var list3 = list.Except(list2).OrderBy(x => x);
 
             Console.Write($"偶數有：{string.Join(",", list2)}");
             Console.Write($"奇數有：{string.Join(",", list3)}");
